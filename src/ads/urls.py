04e84a51f96e3b6ca3dda0f3bca36707dce8a5af
BermetZ <zhumakadyrkyzy.b@gmail.com>
2019-04-01 23:20:12 +0600

    Work in progress for switching to another computer tomorrow.

--- src/ads/urls.py
+++ src/ads/urls.py
@@ -6,11 +6,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.AdList.as_view(), name='ad_list'),
-    path('ad/<int:pk>', views.ad_detail, name='ad_detail'),
+    path('ad/<int:pk>/', views.ad_detail, name='ad_detail'),
     path('ad/new/', views.AdCreate.as_view(), name='ad_new'),
     path('ad/<int:pk>/edit/', views.AdEdit.as_view(), name='ad_edit'),
     path('company/<int:pk>', views.CompanyList.as_view(), name='company_list'),
     path('company/<int:pk>/', views.company_detail, name='company_detail'),
     path('company/new/', views.CompanyCreate.as_view(), name='company_new'),
     path('company/<int:pk>/edit/', views.CompanyEdit.as_view(), name='company_edit'),
+    path('comment/<int:pk>/', views.comment_detail, name='comment_detail'),
+    path('company/new/', views.CommentCreate.as_view(), name='comment_new'),
+    path('company/<int:pk>/edit/', views.CommentEdit.as_view(), name='comment_edit'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
